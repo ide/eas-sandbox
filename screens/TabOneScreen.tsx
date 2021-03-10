24b4fe19e6b8125a211588fc4540467fbe06090f
James Ide <ide@expo.io>
2021-03-10 11:43:03 -0800

    [update] Show the current update manifest on tab 1

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,23 @@
+import * as Updates from 'expo-updates';
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { MonoText } from '../components/StyledText';
 
 export default function TabOneScreen() {
+  const manifest = Updates.manifest;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Current update</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View
+          style={[styles.codeHighlightContainer, styles.updateInfo]}
+          darkColor="rgba(255,255,255,0.05)"
+          lightColor="rgba(0,0,0,0.05)">
+          <MonoText>{manifest}</MonoText>
+        </View>
     </View>
   );
 }
@@ -28,5 +36,12 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  codeHighlightContainer: {
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  updateInfo: {
+    marginVertical: 7,
   },
 });
